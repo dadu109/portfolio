198067078b2d9c6966a0e66df19f504ca1d49eb3
dadu109 <heniek12322@gmail.com>
2020-06-10 22:44:10 +0200

    hotfix

--- dist/portfolio.e31bb0bc.js
+++ dist/portfolio.e31bb0bc.js
@@ -117,7 +117,186 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"index.js":[function(require,module,exports) {
+})({"carousel.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.carousel = void 0;
+
+var carousel = function carousel(_ref) {
+  var container = _ref.container,
+      noSlides = _ref.noSlides,
+      slide = _ref.slide,
+      img = _ref.img,
+      imgWrapper = _ref.imgWrapper,
+      outline = _ref.outline,
+      name = _ref.name,
+      prev = _ref.prev,
+      next = _ref.next,
+      array = _ref.array;
+  var activeSlide = 0;
+  var touchstartY = 0;
+  var touchendY = 0;
+  var switchTlUp = gsap.timeline();
+  var switchTlDown = gsap.timeline({
+    paused: true
+  });
+
+  var update = function update() {
+    slide.innerHTML = activeSlide + 1 < 10 ? "0".concat(activeSlide + 1) : activeSlide + 1;
+    noSlides.innerHTML = array.length < 10 ? "0".concat(array.length) : array.length;
+    img.src = array[activeSlide].imgSrc;
+    name.innerHTML = array[activeSlide].name;
+    outline.innerHTML = array[activeSlide].name;
+    prev.innerHTML = array[activeSlide - 1] ? array[activeSlide - 1].name : array[array.length - 1].name;
+    next.innerHTML = array[activeSlide + 1] ? array[activeSlide + 1].name : array[0].name;
+  };
+
+  update();
+  switchTlDown.to(imgWrapper, 0.3, {
+    opacity: 0,
+    x: '10%',
+    skewType: "simple",
+    skewX: -10
+  }).fromTo(next, {
+    y: -500
+  }, {
+    duration: .3,
+    y: 0
+  }, '-=.3').fromTo(name, {
+    y: -500
+  }, {
+    duration: .3,
+    y: 0
+  }, '-=.3').fromTo(outline, {
+    y: -500
+  }, {
+    duration: .3,
+    y: 0
+  }, '-=.3').fromTo(prev, {
+    y: -500
+  }, {
+    duration: .3,
+    y: 0
+  }, '-=.3').to(imgWrapper, 0.2, {
+    delay: .5,
+    opacity: 1,
+    x: 0,
+    skewType: "simple",
+    skewX: 0
+  });
+  switchTlUp.to(imgWrapper, 0.3, {
+    opacity: 0,
+    x: '10%',
+    skewType: "simple",
+    skewX: -10
+  }).fromTo(next, {
+    y: 500
+  }, {
+    duration: .3,
+    y: 0
+  }, '-=.3').fromTo(name, {
+    y: 500
+  }, {
+    duration: .3,
+    y: 0
+  }, '-=.3').fromTo(outline, {
+    y: 500
+  }, {
+    duration: .3,
+    y: 0
+  }, '-=.3').fromTo(prev, {
+    y: 500
+  }, {
+    duration: .3,
+    y: 0
+  }, '-=.3').to(imgWrapper, 0.2, {
+    delay: .5,
+    opacity: 1,
+    x: 0,
+    skewType: "simple",
+    skewX: 0
+  });
+  container.addEventListener('wheel', function (e) {
+    if (e.deltaY < 0) {
+      if (activeSlide !== 0) {
+        switchTlDown.play(0);
+        activeSlide--;
+      } else {
+        switchTlDown.play(0);
+        activeSlide = array.length - 1;
+      }
+    } else {
+      if (activeSlide !== array.length - 1) {
+        switchTlUp.play(0);
+        activeSlide++;
+      } else {
+        switchTlUp.play(0);
+        activeSlide = 0;
+      }
+    }
+
+    ;
+    update();
+  });
+  container.addEventListener('touchstart', function (event) {
+    touchstartY = event.changedTouches[0].screenY;
+  }, false);
+  container.addEventListener('touchend', function (event) {
+    touchendY = event.changedTouches[0].screenY;
+
+    if (touchendY < touchstartY) {
+      if (activeSlide !== array.length - 1) {
+        switchTlUp.play(0);
+        activeSlide++;
+      } else {
+        switchTlUp.play(0);
+        activeSlide = 0;
+      }
+    } else if (touchendY > touchstartY) {
+      if (activeSlide !== 0) {
+        switchTlDown.play(0);
+        activeSlide--;
+      } else {
+        switchTlDown.play(0);
+        activeSlide = array.length - 1;
+      }
+    }
+
+    update();
+  }, false);
+  prev.addEventListener('click', function () {
+    if (activeSlide !== 0) {
+      switchTlDown.play(0);
+      activeSlide--;
+    } else {
+      switchTlDown.play(0);
+      activeSlide = array.length - 1;
+    }
+
+    update();
+  });
+  next.addEventListener('click', function () {
+    if (activeSlide !== array.length - 1) {
+      switchTlUp.play(0);
+      activeSlide++;
+    } else {
+      switchTlUp.play(0);
+      activeSlide = 0;
+    }
+
+    update();
+  });
+};
+
+exports.carousel = carousel;
+},{}],"index.js":[function(require,module,exports) {
+"use strict";
+
+var _carousel = require("./carousel");
+
 var projects = [{
   name: 'AKTRADE<br>WIZYTÓWKA',
   imgSrc: 'hero.jpg'
@@ -128,26 +307,42 @@ var projects = [{
   name: 'GRA<br>SAPER',
   imgSrc: 'saper.png'
 }];
-var touchstartY = 0;
-var touchendY = 0;
-document.addEventListener('touchstart', function (event) {
-  touchstartY = event.changedTouches[0].screenY;
-}, false);
-document.addEventListener('touchend', function (event) {
-  touchendY = event.changedTouches[0].screenY;
-  handleGesture();
-}, false);
-
-function handleGesture() {
-  if (touchendY < touchstartY) {
-    console.log('Swiped up');
-  } else if (touchendY > touchstartY) {
-    console.log('Swiped down');
-  } else if (touchendY === touchstartY) {
-    console.log('Tap');
-  }
-}
-},{}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+(0, _carousel.carousel)({
+  container: document.querySelector('.carousel'),
+  noSlides: document.querySelector('.carousel .slice-count__no-slides'),
+  slide: document.querySelector('.carousel .slide-count__current-slide'),
+  imgWrapper: document.querySelector('.carousel .active-slide__image-wrapper'),
+  img: document.querySelector('.carousel .active-slide__image'),
+  outline: document.querySelector('.carousel .active-slide__text--outline'),
+  name: document.querySelector('.carousel .active-slide__text--title'),
+  prev: document.querySelector('.carousel .queue-slide--top'),
+  next: document.querySelector('.carousel .queue-slide--bot'),
+  array: projects
+});
+var introAnimation = gsap.timeline();
+introAnimation.fromTo(document.querySelector('.navigation__link--github'), {
+  x: -500
+}, {
+  delay: .5,
+  duration: .3,
+  x: 0
+}).fromTo(document.querySelector('.navigation__link--cv'), {
+  x: 500
+}, {
+  duration: .3,
+  x: 0
+}, '-=.3').fromTo(document.querySelector('.burger'), {
+  x: 500
+}, {
+  duration: .3,
+  x: 0
+}, '-=.3').fromTo(document.querySelector('.navigation__link--logo'), {
+  x: -500
+}, {
+  duration: .3,
+  x: 0
+}, '-=.3');
+},{"./carousel":"carousel.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -175,7 +370,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62327" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55406" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
