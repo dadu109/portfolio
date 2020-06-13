983313acd09441f0aa994e336c21287c195b4c9c
dadu109 <heniek12322@gmail.com>
2020-06-13 14:44:28 +0200

    form succes page

--- dist/carousel.d2a929f1.js
+++ dist/carousel.d2a929f1.js
@@ -352,7 +352,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61953" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64222" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
